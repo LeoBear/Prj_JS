91b82f05e947f902aaddf4bcb42f90cbfb1c3d99
lmedved <lmedved@softserveinc.com>
2016-06-21 23:24:32 +0300

    Refactored, using AngularJS.

--- calc.js
+++ calc.js
@@ -1,65 +1,72 @@
+var calcModel = angular.module("calcApp", []);
 
-		// Should we append digits to the display?  It depends on whether
-		// we've just hit an operation button or a number button.  This
-		// variable keeps track of what we're supposed to do.
-		var appendDigits = false;
+calcModel.controller("CalcCtrl", function ($scope) {
+	
+	// Input data and result of calculation
+	$scope.display = '';
 
-		// In order to successfully calculate, we need to remember the number
-		// that was in the display
-		var prevResult = 0;
+	// Should we append digits to the display?  It depends on whether
+	// we've just hit an operation button or a number button.  This
+	// variable keeps track of what we're supposed to do.
+	$scope.appendDigits = false;
 
-		// Since we're doing a form of infix calculation, we need to remember
-		// the operation.
-		var op = "=";
+	// In order to successfully calculate, we need to remember the number
+	// that was in the display
+	$scope.prevResult = '';
 
-		function clearDisplay()
-		{
-			document.calculator.display.value = 0;
-			prevResult = 0;
-			appendDigits = false;
-			op = "=";
+	// Since we're doing a form of infix calculation, we need to remember
+	// the operation.
+	$scope.op = "=";
+	
+	$scope.clearDisplay = function() {
+		$scope.display = '';
+		$scope.prevResult = '';
+		$scope.appendDigits = false;
+		$scope.op = "=";
+	}
+
+	//   Executes the *previous* operation (e.g., when the user types
+	//   2 + 4 - 2, when the "-" is clicked, the "+" should be executed).
+	$scope.doOp = function( newOperation )
+	{
+		// Get the argument
+		var newArg = eval( $scope.display );
+		
+		// Do the operation
+		if( $scope.op == '+' ) {
+			$scope.prevResult = $scope.prevResult + newArg;
+		}
+		else if( $scope.op == '-' ) {
+			$scope.prevResult = $scope.prevResult - newArg;
+		} 
+		else if( $scope.op == '/' ) {
+			$scope.prevResult = $scope.prevResult / newArg;
+		}
+		else if( $scope.op == '*' ) {
+			$scope.prevResult = $scope.prevResult * newArg;
+		}
+		else if( $scope.op == '=' ) {
+			$scope.prevResult = newArg;
+		}
+		else { // something's wrong
+			$scope.prevResult = newArg;
 		}
 
-		//   Executes the *previous* operation (e.g., when the user types
-		//   2 + 4 - 2, when the "-" is clicked, the "+" should be executed).
-		function doOp( newop )
-		{
-			// Get the argument
-			var newArg = eval( document.calculator.display.value );
-
-			// Do the operation
-			if( op == "+" ) {
-				prevResult = prevResult + newArg;
-			}
-			else if( op == "-" ) {
-				prevResult = prevResult - newArg;
-			} 
-			else if( op == "/" ) {
-				prevResult = prevResult / newArg;
-			}
-			else if( op == "*" ) {
-				prevResult = prevResult * newArg;
-			}
-			else if( op == "=" ) {
-				prevResult = newArg;
-			}
-			else { // something's wrong
-				prevResult = newArg;
-			}
-
-			document.calculator.display.value = prevResult;
-			appendDigits = false;
-			op = newop;
+		$scope.display = $scope.prevResult;
+		$scope.appendDigits = false;
+		$scope.op = newOperation;
+	}
+	
+	// --- Reacts to the user typing a digit --- //
+	$scope.digit = function ( dig )
+	{
+		if( $scope.appendDigits ) {
+			$scope.display += dig;
 		}
-
-		// --- Reacts to the user typing a digit --- //
-		function digit( dig )
-		{
-			if( appendDigits ) {
-				document.calculator.display.value += dig;
-			}
-			else {
-				document.calculator.display.value = dig;
-				appendDigits = true;
-			}
+		else {
+			$scope.display = dig;
+			$scope.appendDigits = true;
 		}
+	}	
+});
+
